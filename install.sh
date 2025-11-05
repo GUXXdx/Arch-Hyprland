9469fb1b3f506ae49526be8b4216a650b76eaac6
JaKooLit <ejhay.games@gmail.com>
2025-11-05 14:00:24 +0100

    feat: graphic redesign - change in style in install.sh - add time duration at the end of the install program - ask confirmation before luching the screen

--- install.sh
+++ install.sh
@@ -1,53 +1,88 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+
+start=$(date +%s)
+PINK="\e[35m"
+WHITE="\e[0m"
+YELLOW="\e[33m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+
 clear
 
-echo "==> WELCOME! Now we will install and setup Hyprland on an Arch-based system"
-echo "==> Create by Phunt_Vieg_, "
-echo "==> Edited by GUXXdx"
+echo -e "
+ ${PINK}\e[1mWELCOME!${PINK} Now we will install and setup Hyprland on an Arch-based system
+                       Created by \e[1;4mPhunt_Vieg_\e[0m
+                          ${PINK}Edited by \e[1;4mGUXXdx${WHITE}
+"
+echo -e "${YELLOW} Do you still want to continue with Hyprland installation using this script? [y/N]: \n"
+
+read -r confirm
+case "$confirm" in
+    [yY][eE][sS]|[yY])
+        echo -e "\n${GREEN}[OK]${PINK} ==> Continuing with installation..."
+        ;;
+    *)
+        echo -e "${BLUE}[NOTE]${PINK} ==> You 🫵 chose ${YELLOW}NOT${PINK} to proceed.. Exiting..."
+        echo
+        exit 1
+        ;;
+esac
 
 cd ~
 
-echo "==> Updating system packages"
+echo -e "${PINK}\n---------------------------------------------------------------------\n${YELLOW}[1/12]${PINK} ==> Updating system packages\n---------------------------------------------------------------------\n${WHITE}"
+sleep 0.4
 sudo pacman -Syu --noconfirm
 
-echo "==> Setup terminal"
+echo -e "${PINK}\n---------------------------------------------------------------------\n${YELLOW}[2/12]${PINK} ==> Setup terminal\n---------------------------------------------------------------------\n${WHITE}"
+sleep 0.4
 chmod +x ~/Arch-Hyprland/arch.sh
 bash ~/Arch-Hyprland/arch.sh
 
-echo "==> Make executable"
+echo -e "${PINK}\n---------------------------------------------------------------------\n${YELLOW}[3/12]${PINK} ==> Make executable\n---------------------------------------------------------------------\n${WHITE}"
+sleep 0.4
 sudo chmod +x ~/dotfiles/.config/guxxdx/*
 
-echo "==> Download wallpaper"
+echo -e "${PINK}\n---------------------------------------------------------------------\n${YELLOW}[4/12]${PINK} ==> Download wallpaper\n---------------------------------------------------------------------\n${WHITE}"
+sleep 0.4
 mkdir -p ~/Pictures/Wallpapers
-mv ~/Arch-Hyprland/Wallpapers/* ~/Pictures/Wallpapers
+cp ~/Arch-Hyprland/Wallpapers/* ~/Pictures/Wallpapers
 rm -rf ~/Arch-Hyprland/Wallpapers/
 
-echo "==> Install package"
+echo -e "${PINK}\n---------------------------------------------------------------------\n${YELLOW}[5/12]${PINK} ==> Install package\n---------------------------------------------------------------------\n${WHITE}"
+sleep 0.4
 ~/dotfiles/.config/guxxdx/install_archpkg.sh
 
-echo "==> Enable bluetooth"
+echo -e "${PINK}\n---------------------------------------------------------------------\n${YELLOW}[6/12]${PINK} ==> Enable bluetooth\n---------------------------------------------------------------------\n${WHITE}"
+sleep 0.4
 sudo systemctl enable --now bluetooth
 
-echo "==> Enable networkmanager"
+echo -e "${PINK}\n---------------------------------------------------------------------\n${YELLOW}[7/12]${PINK} ==> Enable networkmanager\n---------------------------------------------------------------------\n${WHITE}"
+sleep 0.4
 sudo systemctl enable --now NetworkManager
 
-echo "==> Set Ghostty as the default terminal emulator for Nemo"
+echo -e "${PINK}\n---------------------------------------------------------------------\n${YELLOW}[8/12]${PINK} ==> Set Ghostty as the default terminal emulator for Nemo\n---------------------------------------------------------------------\n${WHITE}"
+sleep 0.4
 gsettings set org.cinnamon.desktop.default-applications.terminal exec ghostty
 
-echo "==> Apply fonts"
+echo -e "${PINK}\n---------------------------------------------------------------------\n${YELLOW}[9/12]${PINK} ==> Apply fonts\n---------------------------------------------------------------------\n${WHITE}"
+sleep 0.4
 fc-cache -fv
 
-echo "==> Set cursor"
+echo -e "${PINK}\n---------------------------------------------------------------------\n${YELLOW}[10/12]${PINK} ==> Set cursor\n---------------------------------------------------------------------\n${WHITE}"
+sleep 0.4
 ~/dotfiles/.config/guxxdx/setcursor.sh
 
-echo "==> Stow dotfiles"
+echo -e "${PINK}\n---------------------------------------------------------------------\n${YELLOW}[11/12]${PINK} ==> Stow dotfiles\n---------------------------------------------------------------------\n${WHITE}"
+sleep 0.4
 cd ~/dotfiles
 stow -t ~ .
 cd ~
 
-echo "==> Check display manager"
+echo -e "${PINK}\n---------------------------------------------------------------------\n${YELLOW}[12/12]${PINK} ==> Check display manager\n---------------------------------------------------------------------\n${WHITE}"
+sleep 0.4
 if [[ ! -e /etc/systemd/system/display-manager.service ]]; then
     sudo systemctl enable sddm
     echo -e "[Theme]\nCurrent=sddm-astronaut-theme" | sudo tee -a /etc/sddm.conf
@@ -55,15 +90,26 @@ if [[ ! -e /etc/systemd/system/display-manager.service ]]; then
     echo "SDDM has been enabled."
 fi
 
+end=$(date +%s)
+duration=$((end - start))
+
+hours=$((duration / 3600))
+minutes=$(((duration % 3600) / 60))
+seconds=$((duration % 60))
+
+printf -v minutes "%02d" "$minutes"
+printf -v seconds "%02d" "$seconds"
 
 clear
-echo
-echo "*********************************************************************"
-echo "*                    Hyprland setup is complete!                    *"
-echo "*                                                                   *"
-echo "*   It is recommended to REBOOT your system to apply all changes.   *"
-echo "*                                                                   *"
-echo "*                 Have a great time with Hyprland!!                 *"
-echo "*********************************************************************"
-echo
-echo
+                
+echo -e "\n\n*********************************************************************
+*                    Hyprland setup is complete!                    *
+*                                                                   *
+*             Duration : $hours hours, $minutes minutes, $seconds seconds            *
+*                                                                   *
+*   It is recommended to \e[1;4mREBOOT\e[0m your system to apply all changes.   *
+*                                                                   *
+*                 \e[4mHave a great time with Hyprland!!\e[0m                 *
+*********************************************************************\n\n"
+
+
