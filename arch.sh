7f97af8fdfb221ef095df61f1f6bedd7b9473743
GUXXdx <noah972m@gmail.com>
2026-01-02 15:10:41 +0100

    feat: graphic redesign     - change in style in Arch.sh

--- arch.sh
+++ arch.sh
@@ -1,33 +1,37 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-
+# Variables
+#----------------------------
 # Color variables
-PINK="\e[35m"
+GREEN="\e[32m"
 WHITE="\e[0m"
 YELLOW="\e[33m"
-GREEN="\e[32m"
 BLUE="\e[34m"
+#----------------------------
 
 
 # Welcome message
 echo -e "
  ${PINK}\e[1mWELCOME!${PINK} Now we will install and setup Hyprland on an Arch-based system
                        Created by \e[1;4mPhunt_Vieg_\e[0m
-                          ${PINK}Edited by \e[1;4mGUXXdx$
-{WHITE}"
+                          ${PINK}Edited by \e[1;4mGUXXdx
+${WHITE}"
 
 cd ~
 
-echo "==> Updating system packages..."
+# Updating the system
+echo -e "${GREEN}\n---------------------------------------------------------------------\n${YELLOW}[1/9]${GREEN} ==> Updating system packages\n---------------------------------------------------------------------\n${WHITE}"
 sudo pacman -Syu --noconfirm
 
-echo "==> Setting locale"
+# Setting locale 
+echo -e "${GREEN}\n---------------------------------------------------------------------\n${YELLOW}[2/9]${GREEN} ==> Setting locale \n---------------------------------------------------------------------\n${WHITE}"
 sudo sed -i '/^#en_US.UTF-8 UTF-8/s/^#//' /etc/locale.gen
 sudo locale-gen
 sudo localectl set-locale LANG=en_US.UTF-8
 
-echo "==> Download some terminal tool"
+# Download some terminal tool
+echo -e "${GREEN}\n---------------------------------------------------------------------\n${YELLOW}[3/9]${GREEN} ==> Download some terminal tool\n---------------------------------------------------------------------\n${WHITE}"
 sudo pacman -S --noconfirm --needed base-devel git
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -58,36 +62,43 @@ aur_packages=(
     # CTF tools
     pwninit
 )
+
+# Download pacman packages
+echo -e "${GREEN}\n---------------------------------------------------------------------\n${YELLOW}[4/9]${GREEN} ==> Download pacman packages\n---------------------------------------------------------------------\n${WHITE}"
 sudo pacman -S --noconfirm "${pacman_packages[@]}"
+
+# Download yay packages
+echo -e "${GREEN}\n---------------------------------------------------------------------\n${YELLOW}[5/9]${GREEN} ==> Download yay packages\n---------------------------------------------------------------------\n${WHITE}"
 yay -S --noconfirm "${aur_packages[@]}"
 
-echo "==> Allow pip3 install by removing EXTERNALLY-MANAGED file"
+# Allow pip3 install by removing EXTERNALLY-MANAGED file
+echo -e "${GREEN}\n---------------------------------------------------------------------\n${YELLOW}[6/9]${GREEN} ==> Allow pip3 install by removing EXTERNALLY-MANAGED file\n---------------------------------------------------------------------\n${WHITE}"
 sudo rm -rf $(python3 -c "import sys; print(f'/usr/lib/python{sys.version_info.major}.{sys.version_info.minor}/EXTERNALLY-MANAGED')")
 
-echo "==> Download pwndbg and pwntools"
-git clone --depth=1 https://github.com/pwndbg/pwndbg
-cd pwndbg
-./setup.sh
-cd ..
-pip3 install pwntools
-sudo gem install one_gadget
 
-echo "==> Download file config"
+# Download file config"
+echo -e "${GREEN}\n---------------------------------------------------------------------\n${YELLOW}[7/9]${GREEN} ==> Download file config\n---------------------------------------------------------------------\n${WHITE}"
 git clone --depth=1 https://github.com/GUXXdx/Dotfiles.git ~/dotfiles
 git clone --depth=1 https://github.com/tmux-plugins/tpm ~/dotfiles/.tmux/plugins/tpm
 
-echo "==> Stow"
+# Stow
+echo -e "${GREEN}\n---------------------------------------------------------------------\n${YELLOW}[8/9]${GREEN} ==> Stow\n---------------------------------------------------------------------\n${WHITE}"
 cd ~/dotfiles
 ./.config/guxxdx/backup_config.sh
 stow -t ~ .
 cd ~
 
-echo "==> Change shell"
+# Change shell
+echo -e "${GREEN}\n---------------------------------------------------------------------\n${YELLOW}[9/9]${GREEN} ==> Change shell\n---------------------------------------------------------------------\n${WHITE}"
 ZSH_PATH="$(which zsh)"
 grep -qxF "$ZSH_PATH" /etc/shells || echo "$ZSH_PATH" | sudo tee -a /etc/shells
 chsh -s "$ZSH_PATH"
 
 
-echo
-echo "==> Done!!! Please relogin to apply new config."
-echo
+echo -e "\n ${GREEN}
+ **************************************************
+ *                    \e[1;4mDone\e[0m${GREEN}!!!                     *
+ *       Please relogin to apply new config.      *
+ **************************************************
+ ${WHITE}
+"
